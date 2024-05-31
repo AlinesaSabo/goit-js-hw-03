c2bc82bb4554cd5514fccb62f5888df32ac4ee6b
Alinesa <alinesa322@gmail.com>
2024-05-31 13:05:21 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,15 @@
+function filterArray(numbers, value) {
+  let newNumberArray = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newNumberArray.push(numbers[i]);
+    }
+  }
+
+  return newNumberArray;
+}
+
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
